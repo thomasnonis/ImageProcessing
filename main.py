342e3378a99a9b78792e42bf2065e4d961cfa1a6
thomasnonis <media.thomasnonis@gmail.com>
2021-07-04 16:41:53 +0200

    edit: main module to open examples

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
 import cv2 as cv
 import numpy as np
 
-from core import Image, CameraSource, Curve
-from filters import Kernel, NonLinear, Morph, EdgeDetection
-from functions import concatenate
+import examples
 
-
-
-
-
-
-watch = Image.from_path("samples/watch.png")
-img = CameraSource(0)
-
-watch.get_copy()
-
-tmp = img.get_copy()
-
-while True:
-
-    img.get_next_frame().make_grayscale().filter(EdgeDetection.canny(60,60)).show("Stream")
-    
-
-    if (cv.waitKey(5) & 0xFF == ord('q')):
-        # press 'q' to quit loop
-        break
-
-print("Exited loop")
-img.release_camera()
-
-cv.waitKey(0)   # press any key to quit the program
-
-cv.destroyAllWindows()
+examples.example_2()
